a162cf2135e6ae98d8901caa0927d703383fc3f6
kidkoder432 <61898798+kidkoder432@users.noreply.github.com>
2020-05-18 11:50:47 -0700

    Update hiddenEmail.py

--- Python/hiddenEmail.py
+++ Python/hiddenEmail.py
@@ -1,1 +1,11 @@
 ﻿import pyperclip
+def genRandomText(length):
+    text = []
+    sym = list('`1234567890-=\][poiuytrewqasdfghjkl;'/.,mnbvcxz?><MNBVCXZASDFGHJKL:"}{POIUYTREWQ~!@#$%^&*()_+').append(' ')
+    for i in range(length):
+        text.append(random.choice(sym))
+    return ''.join(text)    
+msg = input('Enter a message > ')
+copy = genRandomText(587349) + '| ' + msg + ' |' + genRandomText(956832)
+pyperclip.copy(copy)
+print('The message has been copied to the clipboard.')
