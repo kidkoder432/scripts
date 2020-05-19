21a851e36d5e2832c792016d4911d52a5b015e75
kidkoder432 <61898798+kidkoder432@users.noreply.github.com>
2020-05-18 21:12:09 -0700

    .

--- Python/shooterGame.py
+++ Python/shooterGame.py
@@ -1,4 +1,11 @@
-import pygame, sys, random
+import pygame, sys, random, time
 from pygame.locals import *
-alien = pygame.load.image('alien.png')
+pygame.init()
+clock = pygame.time.Clock()
+fps = 60
+pmovspd = 2
+bmovspd = 7
+window = pygame.display.set_mode((1000, 00))
+pygame.display.set_caption('Alien Shooter')
+time.sleep(7245)
 
