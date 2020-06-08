2aa28048c2b8432b5564f68ad0e412382d797800
kidkoder432 <61898798+kidkoder432@users.noreply.github.com>
2020-06-08 11:37:16 -0700

    Update saveToUsb.py

--- Python/saveToUsb.py
+++ Python/saveToUsb.py
@@ -1,10 +1,19 @@
 import os, shutil
+if os.path.exists('e:\\backup files'):
+    print('Backup already exists. Please delete the backup folder and re-run the program.')
+    exit(0)
 def copytree(src, dst, symlinks=False, ignore=None):
     for item in os.listdir(src):
         s = os.path.join(src, item)
         d = os.path.join(dst, item)
+        print(item)
         if os.path.isdir(s):
             shutil.copytree(s, d, symlinks, ignore)
         else:
             shutil.copy2(s, d)
-if os.path.exists()
+if os.path.exists('E:\\'):
+    print('Copying...')
+    copytree('c:\\users\\findp\\onedrive\\documents\\prajwal_files', 'e:\\backup files')
+    print('Done.')
+else:
+    print('No drive found.')
