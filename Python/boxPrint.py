29804729ffcf025b7686794240b36567c345ef83
p a <findprajju@gmail.com>
2020-06-09 03:03:13 +0000

    .

--- Python/boxPrint.py
+++ Python/boxPrint.py
@@ -1,7 +1,7 @@
 def boxPrint(symbol, width, height):
     if len(symbol) != 1:
         raise Exception("'symbol' needs to be of length 1")
-    if width + height <= 4:
+    if width < 2 or height < 2:
         raise Exception("'width' and 'height' must be greater than 2")
     print(symbol * width)
     for i in range(height - 2):
