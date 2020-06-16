c19c48b5e69ec074c1934fd61aec5a8b1d441f52
p a <findprajju@gmail.com>
2020-06-16 18:25:55 +0000

    .

---
+++ githelp.sh
@@ -0,0 +1,8 @@
+git config --global credential.helper store
+echo "Run a program by typing its name. If the program is not in the scripts folder, type the name of the folder it is in {case-sensitive}, followed by a forward slash and the script name."
+echo "Pulling..."
+git pull
+# echo "Pushing..."
+# git commit -a -m .
+# git push --set-upstream origin master
+bash
