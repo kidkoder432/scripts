2ffe7c3f5debac34ae1dda4150d16abea07b5f11
kidkoder432 <61898798+kidkoder432@users.noreply.github.com>
2020-07-06 13:45:27 -0700

    Update randomImage.py

--- randomImage.py
+++ randomImage.py
@@ -1,15 +1,17 @@
 from PIL import Image
 import os, secrets
 random = secrets.SystemRandom()
-SIZE = (1000, 1000)
+SIZE = (500, 500)
 path = input('Enter the save location for the random image > ')
 if path:
     os.chdir(path)
-fname = input('Enter the file name > ')
-im = Image.new('RGB', SIZE)
-for w in range(SIZE[0]):
-    for h in range(SIZE[1]):
-        im.putpixel((w, h), (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-print('Image created.')
-im.save(fname)
-print('Stored in directory: %s' %(path))
+nums = list(range(1, 1001)
+for n in nums:
+    fname = n #input('Enter the file name > ')
+    im = Image.new('RGB', SIZE)
+    for w in range(SIZE[0]):
+        for h in range(SIZE[1]):
+            im.putpixel((w, h), (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+    print('Image created.')
+    im.save(fname)
+    print('Stored in directory %s with name %s' %(path, fname))
