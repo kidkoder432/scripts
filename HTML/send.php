38b7b0497eb50478695f6b75d3a3341b2fc34226
kidkoder432 <61898798+kidkoder432@users.noreply.github.com>
2020-07-10 17:16:28 -0700

    .

---
+++ HTML/send.php
@@ -0,0 +1,14 @@
+
+// <html>
+//   <head>
+//     <title>Feedback Sent</title>
+//   </head>
+//   <body>
+//     <?php
+// if($_POST["fdbk"]) {
+//     mail("findprajju@gmail.com", "Feedback from Guess The Raga", $_POST["fdbk"], 'From:' . $_POST['email']); 
+// }
+// echo "<h1>Your feedback has been sent su"
+// ?> 
+//   </body>
+// </html>
