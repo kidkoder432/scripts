089d559f42adc90c68613c075d873b8132182ffa
kidkoder432 <61898798+kidkoder432@users.noreply.github.com>
2020-07-10 17:22:02 -0700

    .

---
+++ backup.bat
@@ -0,0 +1,2 @@
+@py c:\users\findp\onedrive\documents\prajwal_files\scripts\python\savetousb.py %*
+@pause
---
+++ gitcmd.bat
@@ -0,0 +1,5 @@
+cd c:\users\findp\onedrive\documents\prajwal_files\scripts
+git pull
+git add .
+git commit -a -m .
+git push
