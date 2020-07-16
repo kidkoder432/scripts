863f25ac06640a8f5230d3dc5e0d81ed6070a1f5
kidkoder432 <findprajju@gmail.com>
2020-07-16 11:46:42 -0700

    update

--- Python/tesseractdemo.py
+++ Python/tesseractdemo.py
@@ -3,11 +3,5 @@ try:
 except ImportError:
     import Image
 import pytesseract
-
-# If you don't have tesseract executable in your PATH, include the following:
-pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
-# Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
-
-# Simple image to string
 print(pytesseract.image_to_string(Image.open('test.png')))
 
