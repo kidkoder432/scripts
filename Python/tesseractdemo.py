589d25a950803e40b91614f35e56ae26699de2f8
kidkoder432 <findprajju@gmail.com>
2020-07-17 10:03:06 -0700

    update

--- Python/tesseractdemo.py
+++ Python/tesseractdemo.py
@@ -1,7 +1,11 @@
+import os
 try:
     from PIL import Image
 except:
 	import Image
 import pytesseract
-print(pytesseract.image_to_string(Image.open(input('Enter file to open: '))))
+ch = input('Enter the directory the file is in (or "here" for this directory: ')
+if ch != 'here':
+	os.chdir(ch)
+print(pytesseract.image_to_string(Image.open(input('Enter filename: '))))
 
