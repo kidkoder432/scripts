7c0701ee72991ee8f43a2fec90015db7784f46cb
kidkoder432 <findprajju@gmail.com>
2020-07-17 11:42:49 -0700

    update

---
+++ Python/happyOrSad.py
@@ -0,0 +1,9 @@
+from myai import *
+feeling = input('Hello, %s. How are you today? > ' %(input('Hello. What is your name? > '))).lower()
+yn = getYesNo(feeling)
+if yn == 0 and len(feeling.split(' ')) > 1:
+    print('''I'm sorry you feel that way. Is there anything I can do to help?''')
+elif len(feeling) == 0:
+    feeling = input('Oops. I didn\'t get that. Could you say it again? > ').lower()    
+else:
+    print('I think we\'ll make good friends.')
