471a959d2e12dcf42b3c99ffc9f59d2df979e7f6
kidkoder432 <findprajju@gmail.com>
2020-07-30 14:21:20 -0700

    .

--- Python/addIndex.py
+++ Python/addIndex.py
@@ -1,5 +1,9 @@
 ragas = open('ragas.csv')
-rindex = open('raga-index.txt', w)
+rindex = open('raga-index.txt', 'w')
 for r in str(ragas.read()).split('\n'):
-    rindex.write(r.split(',')[0] + ': ' + r.split(',')[1] + ' '  r.split(',')[2])
-
+    print(r)
+    if r:
+        rindex.write(r.split(',')[0] + ': ' + r.split(',')[1] + ' ' + r.split(',')[2])
+        rindex.write('\n')
+ragas.close()
+rindex.close()
