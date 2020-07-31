28ee55f871d01aff0b35c066f3d3934d5f4dcab9
kidkoder432 <findprajju@gmail.com>
2020-07-31 10:47:02 -0700

    .

--- Python/typePrint.py
+++ Python/typePrint.py
@@ -1,13 +1,12 @@
-def typePrint(val, speed):
-    import time
-    val = list(val)
-    print()
-    for letter in val:
-        print(letter, end = ' ')
-        time.sleep(1)
-    print('\n')
+
+
+import time
 while True:
     val = input('Please enter a string of characters. >')
     speed = float(input('Please enter the timing between each letter. >'))
-    typePrint(val, speed)
- 
+    val = list(val)
+    print()
+    for letter in val:
+        print('\r' + letter)
+        time.sleep(speed)
+    print('\n')
