1a1c6e88d777caab6572e5e172db8b7918ed351d
kidkoder432 <findprajju@gmail.com>
2020-08-04 10:50:09 -0700

    .

--- Python/musicwidgets.py
+++ Python/musicwidgets.py
@@ -1,4 +1,4 @@
-import winsound as w, time
+import os, time
 def mlinput(prompt):
     ui = None
     print(prompt)
@@ -7,6 +7,23 @@ def mlinput(prompt):
         ui = input()
         totalui.append(ui)
     return '\n'.join(totalui)  
+SCALE = {'S': 208 	
+'r': 220
+'R': 233 	
+'g': 247 	
+'G': 262 	
+'M': 277 
+'m': 294 	
+'P': 311 	
+'d': 330 	
+'D': 349 	
+'n': 370 	
+'N': 392}
+def play(f, d):
+    '''f: frequency in Hz\n
+    d: duration in milliseconds'''
+    os.system('play -n --no-show-progress synth %s sin %s' % (d / 1000, f))
+
 def musicMaker():
     print('''Welcome to MusicMaker!
              Syntax:
@@ -16,7 +33,7 @@ def musicMaker():
     for n in (list(SCALE.keys())):
         if n.isupper():
             print(n)
-            w.Beep(SCALE.get(n), 1000)
+            play(SCALE.get(n), 1000)
     while True:
         seq = mlinput('Please enter a sequence of notes. >>> ')
         for note in seq.split('  '):
@@ -24,11 +41,11 @@ def musicMaker():
             for n in note.split(' '):
                 try:
                     if len(note) > 1:
-                        w.Beep(SCALE.get(n), 150)
+                        play(SCALE.get(n), 150)
                     elif seq[seq.index(note) + 1] == '-': 
-                        w.Beep(SCALE.get(n), 600)
+                        play(SCALE.get(n), 600)
                     else:
-                        w.Beep(SCALE.get(n), 300)
+                        play(SCALE.get(n), 300)
                 except (TypeError, IndexError):
                     pass
 musicMaker()        
