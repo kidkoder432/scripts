0289c2f52a881f8b9e408c8d23c95575d158c335
kidkoder432 <findprajju@gmail.com>
2020-08-04 10:05:29 -0700

    .

--- Python/timer.py
+++ Python/timer.py
@@ -3,7 +3,9 @@ import os
 frequency = 880
 time = int(input('How many seconds to count down?'))
 print(time)
-for i in range(time * 1000):
-    print('Time left: %s' %((time * 1000 - i) / 1000), end='\r')
-    #os.system('play -n --no-show-progress synth %s sin %s' % ((time * 1000 - i) / 1000, frequency))
-    sleep((time * 1000 - i) / 1000)
+dur = 1
+for i in range(time * 10):
+    print('Time left: %s' %((time - i)), end='\r')
+    os.system('play -n --no-show-progress synth %s sin %s' % (dur / 2, frequency))
+    sleep(dur / 2)
+    dur /= 1.01
