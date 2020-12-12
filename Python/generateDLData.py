216d6bc21e6368e2dc438910e4f66f5f9cca828f
= <=>
2020-12-11 22:16:31 -0800

    Added js ping pong / website: http://pong.prajwalagrawal.repl.co

--- Python/generateDLData.py
+++ Python/generateDLData.py
@@ -1,45 +1,39 @@
+import multiprocessing
 print('Phase 1: Generating data...')
-import platform, os
-from guessRaga import getRagas, loadDictionary
-DATA = {}
+import itertools, random
+l = list(itertools.product(list('SRGMPDNrgmdn'), repeat=5))# + list(itertools.product(list('SRGMPDNrgmdn'), repeat=6))
+from guessRaga import *
 def isValid(phrase):
     for x in range(len(phrase) - 1):
-        if phrase[x].lower() == phrase[x+1].lower():
+        if phrase[x].lower() == phrase[x+1].lower() and ((phrase[x].islower() and phrase[x+1].isupper()) or (phrase[x].isupper() and phrase[x+1].islower())):
             return False
     return True
+
+def op(phrase):
+    gs = None
+    if isValid(phrase):
+        gs = getRagas(phrase, 0.7)
+        
+    if gs and len(gs.split(' or ')) == 1:
+        return [phrase, gs]
+        
 def conv(phrase):
-    t = 0
-    multiplier = 10 ** (len(phrase) - 1)
-    notes = ['','S','r','R','g','G','M','m','P','d','D','n','N']
-    for n in phrase:
-        t += notes.index(n) * multiplier
-        multiplier /= 10
-    return t
-import itertools, random
-print("Generating and evaluatiing phrases...")
-PHRASES = [list(p) for p in itertools.product(['S', 'r', 'R', 'g', 'G', 'M', 'm', 'P', 'd', 'D', 'n', 'N'], repeat=5) if isValid(p)]
-for j in PHRASES:
-    SAPTAKS = loadDictionary()
-    print('Evaluating phrase: ' + ''.join(j))
-    guesses = getRagas(SAPTAKS, ''.join(j ), 0.72, '/')
-    if guesses:
-        DATA[''.join(j)] = guesses
-for raga in list(SAPTAKS.keys()):
-    print('Writing data...')
-    fname = 'is' + raga[:raga.index(':')] + '.csv'
-    f = open(fname, 'w+')
-    for k in list(DATA.keys()):
-        print(DATA[k], raga[:raga.index(':')])
-        if raga[:raga.index(':')] in DATA[k]:
-            f.write(str(conv(''.join(k))) + ', ' + '1')
-        else:
-            f.write(str(conv(''.join(k))) + ', ' + '0')
-        f.write('\n')
+    n = NOTES
+    o = []
+    for p in phrase:
+        o.append(str(n.index(p)))
+    return ', '.join(o)
+if __name__ == '__main__':
+    gs = 0
+    import time
+    t = time.time()
+    with multiprocessing.Pool(4) as pool:
+        d = pool.map(op, l)
 
-
-
-
+    f = open('dldata.csv', 'w')
+    for x in d:
+        if x:
+            f.write('\n' + conv(x[0]) + ', ' + str(RAGAS.index(x[1])))
     f.close()
-    print('Done.')
-    print('Collected ' + str(len(DATA)) + ' entries')
-print('Finished operations.')
+    e = time.time()
+    print(e-t)
