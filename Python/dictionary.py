2bc25e4dac7f467ec9d0b566a73a428b26010ccf
= <=>
2021-06-22 08:38:57 -0700

    updates

--- Python/dictionary.py
+++ Python/dictionary.py
@@ -1,11 +1,22 @@
-#! python3
-# downloadXkcd.py - Downloads every single XKCD comic.
+# #! python3
+# # downloadXkcd.py - Downloads every single XKCD comic.
+import bs4, requests
 
-import PyDictionary as pyd
+# import PyDictionary as pyd
 
-words = pyd.PyDictionary()
+# words = pyd.PyDictionary()
+# while True:
+#     word = input('Enter a word: ')
+#     print(words.meaning(word))
 while True:
-    word = input('Enter a word: ')
-    print(words.meaning(word))
+    word = input("Enter a word: ")
+    url = f"https://www.merriam-webster.com/dictionary/{word}"
+    res = requests.get(url)
+    res.raise_for_status()
 
+    soup = bs4.BeautifulSoup(res.text, "html.parser")
 
+    elem = soup.select(
+        "#dictionary-entry-1 > div.vg > div > span"
+    )
+    print(elem[0].text)
